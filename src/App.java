b11f42d18d53594dda3582b90371f36fc405d735
unknown <danileron@gmail.com>
2018-07-26 16:00:33 +0300

    FixedThreadPool

--- src/App.java
+++ src/App.java
@@ -1,9 +1,47 @@
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
     public static void main(String[] args) {
-        new Worker().main();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for(int i = 0;i < 5;i++) {
+            executorService.submit(new Processor(i));
+        }
+
+        executorService.shutdown();
+        System.out.println("All tasks submitted");
+        try {
+            executorService.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("All tasks completed");
     }
 
+}
+
+class Processor implements Runnable{
+
+    private int id;
+
+    public Processor(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Starting : " + id);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Terminating : " + id);
+    }
 }
