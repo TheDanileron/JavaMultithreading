720fee38f0c8289aa751d09a989827df6704884c
unknown <danileron@gmail.com>
2018-07-26 12:20:34 +0300

    Stopping Thread

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Runner runner = new Runner(1);
-        runner.start();
+        Process process = new Process();
+        process.start();
 
-        Runner runner1 = new Runner(2);
-        runner1.start();
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        process.shutDown();
     }
 }
 
-class Runner extends Thread {
-
-    public int runnerNumber;
-
-    public Runner(int runnerNumber) {
-        this.runnerNumber = runnerNumber;
-    }
+class Process extends Thread{
+    // Each thread caches the values of variables from the memory. Thus the thread does not have to always refer the memory,
+    // and it can simply read the values from the cache. This is gives rise to the problem of visibility.
+    // This cache may not be consistent with what other variables see. With volatile, we skip the cache and read/write
+    // directly in the memory. Thus, changes are visible to all threads.﻿
+    // To make sure current Thread will see the changes made by other Thread we must skip cache using volatile
+    private volatile boolean isRunning = true;
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+
+        while (isRunning) {
             try {
-                sleep(2000);
+                sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("Runner " + this.runnerNumber + " runs for " + i + " seconds");
+            System.out.println("I'm running");
         }
+    }
+
+    void shutDown() {
+        this.isRunning = false;
     }
 }
