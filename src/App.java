8bb1f17de6a5eb0b7de1bcc02587ab722ca3ff59
unknown <danileron@gmail.com>
2018-07-27 16:19:58 +0300

    Semaphores

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -6,32 +7,41 @@ import java.util.concurrent.locks.ReentrantLock;
 public class App {
 
     public static void main(String[] args) {
-        Runner runner = new Runner();
-        Thread threadFirst = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.firstThreadMethod();
-            }
-        });
+        // Semaphore maintains a count of available permits
+        Semaphore semaphore = new Semaphore(1, true);
 
-        Thread threadSecond = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.secondThreadMethod();
-            }
-        });
-
-        threadFirst.start();
-        threadSecond.start();
+        // Increments the number of available permits
+        semaphore.release();
 
         try {
-            threadFirst.join();
-            threadSecond.join();
+            // Decrements the number of available permits
+            // if semaphore has no permits the require will pause current thread until some permit is available
+            semaphore.acquire();
+            semaphore.acquire();
+//            semaphore.acquire();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        runner.finish();
+        System.out.println("Semaphore available permits : " + semaphore.availablePermits());
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    Connection.getInstance().tryConnect();
+                }
+            });
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
