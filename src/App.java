d5412de18045056e326f54f1902bca600a058348
unknown <danileron@gmail.com>
2018-08-14 15:27:52 +0300

    CompletableFuture

--- src/App.java
+++ src/App.java
@@ -4,38 +4,36 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+import static java.lang.String.format;
+
 public class App {
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newCachedThreadPool();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        System.out.println("Starting ...");
+        ExecutorService threadPool = Executors.newFixedThreadPool(8);
 
-        // Callable can return a value wrapped in a Future object, that has the same value type as value
-        // specified in Callable. When we submit callable it returns a Future object;
-        Future<Integer> result = executorService.submit(new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                Random random = new Random();
-                int duration = random.nextInt(4000);
+        Counter counter = new Counter();
+        long start = System.nanoTime();
 
-                System.out.println("Callable started ...");
-                Thread.sleep(duration);
-                System.out.println("Callable done its work. ");
+        List<Future<Double>> futures = new ArrayList<>();
+        for (int i = 0; i < 400; i++) {
+            final int j = i;
 
-                return duration;
-            }
-        });
-
-        executorService.shutdown();
-
-        try {
-            // Future get method will wait until the Callable is terminated
-            System.out.println("Time wasted on task : " + result.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            // get throws ExecutionException if Callable throws any exception
-            e.printStackTrace();
+            futures.add(CompletableFuture.supplyAsync(
+                    () -> counter.count(j),
+                    threadPool
+            ));
         }
+
+        double value = 0;
+        for (Future<Double> future : futures) {
+            value += future.get();
+        }
+
+        System.out.println(format("Executed by %d s, value : %f",
+                (System.nanoTime() - start) / (1000_000_000),
+                value));
+        threadPool.shutdown();
     }
 
 }
